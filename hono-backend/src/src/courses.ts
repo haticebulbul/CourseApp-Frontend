3f0610f913466188b2077408920b5a29cb8a558a
haticebulbul <hatice.bulbul@ogr.sakarya.edu.tr>
2024-09-11 14:32:32 +0300

    .

---
+++ hono-backend/src/src/courses.ts
@@ -0,0 +1,38 @@
+export const courses = [
+    {
+      title: "Beginner Full Stack Web Development: HTML, CSS, React & Node",
+      instructors: ["Mark Wahlbeck", "Developes by Nathan"],
+      rating: 4.5,
+      reviewCount: 12549,
+      imageUrl: "path/to/beginner-fullstack-course.jpg"
+    },
+    {
+      title: "The Ultimate 2024 Fullstack Web Development Bootcamp",
+      instructors: ["Kane Ezki", "Kalob Taulien", "Radek Wilk"],
+      rating: 4.6,
+      reviewCount: 7520,
+      imageUrl: "path/to/ultimate-2024-bootcamp.jpg"
+    },
+    {
+      title: "The Full Stack Web Development Bootcamp 2024",
+      instructors: ["Masynctech Coding School", "Aditya Mandal"],
+      rating: 4.4,
+      reviewCount: 2407,
+      imageUrl: "path/to/fullstack-bootcamp-2024.jpg"
+    },
+    {
+      title: "Crash Course: Build a Full-Stack Web App in a Weekend!",
+      instructors: ["Jonas Schmedtmann"],
+      rating: 4.8,
+      reviewCount: 2981,
+      imageUrl: "path/to/crash-course-fullstack.jpg"
+    },
+    {
+      title: "Laravel - For Beginner to Advanced",
+      instructors: ["Web Solution US"],
+      rating: 4.2,
+      reviewCount: 324,
+      imageUrl: "path/to/laravel-beginner-advanced.jpg"
+    }
+  ];
+  
